f3f980dfd5ff5b6155406bec5cce124fd02b4f68
Stephen Sherratt <stephen@sherra.tt>
2021-11-10 01:40:35 +1100

    Add missing App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as model from './model';
+import { ButtonMenuStrip } from './view';
+
+type UiState = model.ButtonMenuStrip;
+
+interface RootState {
+  uiState: UiState,
+}
+
+interface RootProps {
+  initialState: RootState,
+}
+
+export class App extends React.Component<RootProps, RootState> {
+  constructor(props: RootProps) {
+    super(props);
+    this.state = props.initialState;
+  }
+
+  render(): JSX.Element {
+    const updateButtonMenuStrip = (uiState: UiState): void => {
+      this.setState({
+        uiState
+      });
+    };
+    const { uiState } = this.state;
+    return <>
+      { ButtonMenuStrip({ ...uiState, updateSelf: updateButtonMenuStrip }) }
+    </>;
+  }
+}
