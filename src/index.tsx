9e3cf9698712e4312bd77f8ca450273c64cd0a38
Stephen Sherratt <stephen@sherra.tt>
2021-11-10 01:28:43 +1100

    Remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, AnyAction } from 'redux';
-import { Provider, useSelector, useDispatch } from 'react-redux';
 import { List } from 'immutable';
 import * as model from './model';
 import { shortcutText } from './model';
-import { ButtonMenuStrip } from './view';
+import { App } from './App';
 
 const BUTTON_MENU_STRIP: model.ButtonMenuStrip = {
   buttonMenus: List([
@@ -37,53 +35,13 @@ const BUTTON_MENU_STRIP: model.ButtonMenuStrip = {
   ])
 };
 
-type UiState = model.ButtonMenuStrip;
-
-interface RootState {
-  uiState: UiState,
-}
-
 const ROOT_STATE_DEFAULT = {
   uiState: BUTTON_MENU_STRIP,
 };
 
-interface SetUiStateAction extends AnyAction {
-  type: 'set-ui-state',
-  uiState: UiState,
-}
-
-function matchSetUiStateAction(action: AnyAction): action is SetUiStateAction {
-  return action.type === 'set-ui-state';
-}
-
-function reducer(state: RootState = ROOT_STATE_DEFAULT, action: AnyAction): RootState {
-  if (matchSetUiStateAction(action)) {
-    return { ...state, uiState: action.uiState };
-  }
-  return state;
-}
-
-const store = createStore(reducer);
-
-function App(): JSX.Element {
-  const { uiState } = useSelector((state: RootState) => state);
-  const dispatch = useDispatch();
-  function updateButtonMenuStrip(buttonMenuStrip: model.ButtonMenuStrip): void {
-    dispatch({
-      type: 'set-ui-state',
-      uiState: buttonMenuStrip,
-    });
-  }
-  return <>
-    { ButtonMenuStrip({ ...uiState, updateSelf: updateButtonMenuStrip }) }
-  </>;
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App initialState={ROOT_STATE_DEFAULT} />
   </React.StrictMode>,
   document.getElementById('root'),
 );
